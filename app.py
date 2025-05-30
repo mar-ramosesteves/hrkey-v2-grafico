a37e4b44c3c2e935ff95be6d7819b49d049f1aa9
mar-ramosesteves <mar.ramosesteves@gmail.com>
2025-05-30 17:41:41 -0300

    Update app.py

--- app.py
+++ app.py
@@ -40,6 +40,8 @@ def home():
 @app.route("/gerar-relatorio-json", methods=["POST"])
 def gerar_relatorio_json():
     try:
+    print("📥 Iniciando processamento da rota /gerar-graficos-comparativos")    
+
         dados = request.get_json()
         empresa = dados.get("empresa")
         codrodada = dados.get("codrodada")
