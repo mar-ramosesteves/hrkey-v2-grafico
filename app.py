81a373fa75a159b3c46c694fe230ffd6f4b8ff20
mar-ramosesteves <mar.ramosesteves@gmail.com>
2025-07-20 12:09:20 -0300

    Update app.py

--- app.py
+++ app.py
@@ -423,7 +423,7 @@ def gerar_graficos_comparativos():
         payload = {
             "empresa": empresa,
             "codrodada": codrodada,
-            "emailLider": emailLider,
+            "emaillider": emailLider,
             "data_criacao": datetime.now().isoformat(),
             "nome_arquivo": nome_arquivo,
             "dados_json": dados_ia,
@@ -762,7 +762,7 @@ def salvar_json_ia_no_supabase(dados_ia, empresa, codrodada, emailLider, nome_ar
     payload = {
         "empresa": empresa,
         "codrodada": codrodada,
-        "emailLider": emailLider,
+        "emaillider": emailLider,
         "dados_json": dados_ia,
         "nome_arquivo": nome_arquivo,
         "data_criacao": datetime.now().isoformat()
